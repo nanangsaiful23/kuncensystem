383fcd854b719b3c159234404703611483fef20c
nisadyo <riyaniannisa@yahoo.com>
2023-08-04 15:19:41 +0700

    fixing form account

--- resources/views/layout/account/form.blade.php
+++ resources/views/layout/account/form.blade.php
@@ -1,12 +1,71 @@
 <div class="panel-body" style="color: black !important;">
     <div class="row">
         <div class="form-group">
+            {!! Form::label('code', 'Kode Akun', array('class' => 'col-sm-12')) !!}
+            <div class="col-sm-5">
+                @if($SubmitButtonText == 'View')
+                    {!! Form::text('code', null, array('class' => 'form-control', 'readonly' => 'readonly')) !!}
+                @else
+                    {!! Form::text('code', null, array('class' => 'form-control')) !!}
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group">
             {!! Form::label('name', 'Nama Akun', array('class' => 'col-sm-12')) !!}
             <div class="col-sm-5">
                 @if($SubmitButtonText == 'View')
                     {!! Form::text('name', null, array('class' => 'form-control', 'readonly' => 'readonly')) !!}
                 @else
                     {!! Form::text('name', null, array('class' => 'form-control')) !!}
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('type', 'Tipe Akun', array('class' => 'col-sm-12')) !!}
+            <div class="col-sm-5">
+                @if($SubmitButtonText == 'View')
+                    {!! Form::text('type', null, array('class' => 'form-control', 'readonly' => 'readonly')) !!}
+                @else
+                    <select class="form-control select2" style="width: 100%;" name="payment">
+                        <div>
+                            <option value="Debet">Debet</option>
+                            <option value="Kredit">Kredit</option>
+                        </div>
+                    </select>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('group', 'Grup Akun', array('class' => 'col-sm-12')) !!}
+            <div class="col-sm-5">
+                @if($SubmitButtonText == 'View')
+                    {!! Form::text('group', null, array('class' => 'form-control', 'readonly' => 'readonly')) !!}
+                @else
+                    <select class="form-control select2" style="width: 100%;" name="payment">
+                        <div>
+                            <option value="Neraca">Neraca</option>
+                            <option value="Laba Rugi">Laba Rugi</option>
+                        </div>
+                    </select>
+                @endif
+            </div>
+        </div>
+
+        <div class="form-activa">
+            {!! Form::label('activa', 'Tipe Akun', array('class' => 'col-sm-12')) !!}
+            <div class="col-sm-5">
+                @if($SubmitButtonText == 'View')
+                    {!! Form::text('activa', null, array('class' => 'form-control', 'readonly' => 'readonly')) !!}
+                @else
+                    <select class="form-control select2" style="width: 100%;" name="payment">
+                        <div>
+                            <option value="Activa">Aktiva</option>
+                            <option value="Pasiva">Pasiva</option>
+                        </div>
+                    </select>
                 @endif
             </div>
         </div>
