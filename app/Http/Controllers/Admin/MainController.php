c8d11c485e5a47214f970f6a0a3b7e6a39ae22c9
nisadyo <riyaniannisa@yahoo.com>
2024-09-02 11:12:23 +0700

    fixing neraca

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -170,43 +170,43 @@ class MainController extends Controller
     {
         $default['page_name'] = 'Neraca';
 
-        $activa_debits = Journal::select(DB::raw('SUM(journals.debit) as debit'), 'accounts.code', 'accounts.name', 'accounts.balance')
+        $activa_debits = Journal::select(DB::raw('SUM(journals.debit) as debit'), 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->rightJoin('accounts', 'accounts.id', 'journals.debit_account_id')
                         ->where('accounts.activa', 'aktiva')
                         ->where('accounts.deleted_at', null)
                         ->whereDate('journals.journal_date', '>=', $start_date) 
                         ->whereDate('journals.journal_date', '<=', $end_date) 
-                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance')
+                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->orderBy('accounts.code')
                         ->get();
 
-        $activa_credits = Journal::select(DB::raw('SUM(journals.credit) as credit'), 'accounts.code', 'accounts.name', 'accounts.balance')
+        $activa_credits = Journal::select(DB::raw('SUM(journals.credit) as credit'), 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->rightJoin('accounts', 'accounts.id', 'journals.credit_account_id')
                         ->where('accounts.activa', 'aktiva')
                         ->where('accounts.deleted_at', null)
                         ->whereDate('journals.journal_date', '>=', $start_date) 
                         ->whereDate('journals.journal_date', '<=', $end_date) 
-                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance')
+                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->orderBy('accounts.code')
                         ->get();
 
-        $pasiva_debits = Journal::select(DB::raw('SUM(journals.debit) as debit'), 'accounts.code', 'accounts.name', 'accounts.balance')
+        $pasiva_debits = Journal::select(DB::raw('SUM(journals.debit) as debit'), 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->rightJoin('accounts', 'accounts.id', 'journals.debit_account_id')
                         ->where('accounts.activa', 'pasiva')
                         ->where('accounts.deleted_at', null)
                         ->whereDate('journals.journal_date', '>=', $start_date) 
                         ->whereDate('journals.journal_date', '<=', $end_date) 
-                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance')
+                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->orderBy('accounts.code')
                         ->get();
 
-        $pasiva_credits = Journal::select(DB::raw('SUM(journals.credit) as credit'), 'accounts.code', 'accounts.name', 'accounts.balance')
+        $pasiva_credits = Journal::select(DB::raw('SUM(journals.credit) as credit'), 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->rightJoin('accounts', 'accounts.id', 'journals.credit_account_id')
                         ->where('accounts.activa', 'pasiva')
                         ->where('accounts.deleted_at', null)
                         ->whereDate('journals.journal_date', '>=', $start_date) 
                         ->whereDate('journals.journal_date', '<=', $end_date) 
-                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance')
+                        ->groupBy('accounts.id', 'accounts.code', 'accounts.name', 'accounts.balance', 'journals.journal_date')
                         ->orderBy('accounts.code')
                         ->get();
 
