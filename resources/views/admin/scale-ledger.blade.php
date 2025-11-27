ae047f0bebb4f27695583429f8553f3909ce93d6
nisadyo <riyaniannisa@yahoo.com>
2025-11-27 20:16:51 +0700

    try changing scale ledger ongoing

--- resources/views/admin/scale-ledger.blade.php
+++ resources/views/admin/scale-ledger.blade.php
@@ -1,3 +1,11 @@
+<style>
+  .select2-container--default .select2-selection--multiple .select2-selection__choice 
+  {
+    background-color: yellow !important;
+    color: black !important;
+  }
+</style>
+
 @extends('layout.user', ['role' => 'admin', 'title' => 'Admin'])
 
 @section('content')
@@ -22,6 +30,10 @@
                 <div class="input-group date">
                   <input type="text" class="form-control pull-right" name="end_date" value="{{ $end_date }}" id='end_date' onchange="advanceSearch()">
                 </div>
+              </div>
+              {!! Form::label('account', 'Jenis', array('class' => 'col-sm-1 control-label')) !!}
+              <div class="col-sm-3">
+                {!! Form::select('account', getAccountLists(), null, ['class' => 'form-control select2', 'multiple' => 'multiple', 'style'=>'width: 100%', 'id' => 'account', 'onchange' => 'changeGraph()']) !!}
               </div>
               <div class="chart" id="line-chart" style="height: 500px;"></div>
             </div>
@@ -57,42 +69,66 @@
             ajaxFunction();
         });
     });
-
-var line = new Morris.Line({
-      element: 'line-chart',
-      resize: true,
-      data: [ 
-        @for($i = 0; $i < sizeof($dates); $i++)
-            <?php $show = ''; ?>
-            @for($j = 0; $j < sizeof($dates[$i]->data); $j++)
-              <?php $show .= $dates[$i]->data[$j]->code . ":" . ($dates[$i]->data[$j]->current/1000) . ","; ?>
-            @endfor
-          {x: '{{ $dates[$i]->date }}', {{ $show }}},
-        @endfor
-      ],
-      xkey: 'x',
-      ykeys: [
-        @for($j = 0; $j < sizeof($dates[0]->data); $j++)
-          "{{ $dates[0]->data[$j]->code }}",
-        @endfor
-      ],
-      xLabelAngle: 60,
-      labels: [
-          @for($j = 0; $j < sizeof($dates[0]->data); $j++)
-            "{{ $dates[0]->data[$j]->name }}",
-          @endfor
-      ],
-      lineColors: [
-          @for($j = 0; $j < sizeof($dates[0]->data); $j++)
-            "{{ $dates[0]->data[$j]->color }}",
-          @endfor],
-      hideHover: 'auto',
-      parseTime: false
-      // ymin: 0,   // Set the minimum Y-axis value
-      // ymax: 100000000
-    });
     
 
+    function changeGraph() 
+    {
+      ykeysgraph = [];
+      labelsgraph = [];
+      colorsgraph = [];
+      showall = [];
+      dates = <?php echo json_encode($dates); ?>;
+      dates = dates.data;
+      for(i = 0; i < dates.length; i++)
+      {
+          show = {};
+          date = dates[i].data.data;
+          for(j = 0; j < date.length; j++)
+          {
+            data = date[j].code;
+            accounts = $('#account').val();
+            if(accounts.includes(data))
+            {
+              show[data] = (date[j].current / 1000); 
+
+              if(!ykeysgraph.includes(data))
+                ykeysgraph.push(data);
+              if(!labelsgraph.includes(date[j].name))
+                labelsgraph.push(date[j].name);
+              if(!colorsgraph.includes(date[j].color))
+                colorsgraph.push(date[j].color);
+            }
+          }
+          if(show != '')
+          {
+            show['x'] = dates[i].date; 
+            showall.push(show);
+          }
+      }
+
+      console.log(showall);
+      console.log(ykeysgraph);
+      console.log(labelsgraph);
+      console.log(colorsgraph);
+
+      var line = new Morris.Line({
+        element: 'line-chart',
+        resize: true,
+        data: showall,
+        xkey: 'x',
+        ykeys: ykeysgraph,
+        xLabelAngle: 60,
+        labels: labelsgraph,
+        lineColors: colorsgraph,
+        hideHover: 'auto',
+        parseTime: false
+      });
+
+      line.options.ykeys.push(ykeysgraph);
+    line.options.labels.push(labelsgraph);
+    line.options.lineColors.push(colorsgraph); // Add a new color
+    line.redraw();
+    }
     function advanceSearch()
     {
       window.location = window.location.origin + '/admin/scaleLedger/' + $('#start_date').val() + '/' + $('#end_date').val() + '/' + $('#account_code').val();
